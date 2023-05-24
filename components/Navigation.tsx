09ad6b5e5a916fb20e8509e75486b5e0e3f127b5
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-05-24 21:51:01 +0300

    aside navlink active

---
+++ components/Navigation.tsx
@@ -0,0 +1,30 @@
+"use client"
+import Link from "next/link"
+import  {usePathname} from "next/navigation";
+import styles from '@/app/styles/navigation.module.scss';
+type NavLink = {
+    label: string,
+    href: string
+};
+type Props = {
+    navLinks: NavLink[];
+}
+
+const Navigation = ({navLinks}: Props) =>{
+    const pathname = usePathname();
+
+    return <div className={styles.aside_menu}>
+        {navLinks.map((link)=> {
+            const isActive = pathname ===link.href;
+
+            return <Link
+            className={isActive ? styles.aside_menu_item_active : styles.aside_menu_item}
+            key = {link.label}
+            href = {link.href}>
+                {link.label}
+            </Link>
+        })}
+    
+    </div>
+}
+export {Navigation}
