9aa6928ad8cd9083a67c995a49e301743fbe5ee9
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-06-07 21:47:46 +0300

    added all goods

---
+++ app/tablets/[id]/page.tsx
@@ -0,0 +1,77 @@
+"use client"
+import styles from '../../styles/good.module.scss';
+import { getItem } from '@/services/getGoods';
+import { useEffect, useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setGoods, addToBasket } from '@/store/goodsSlice';
+import YouBuyIT from '@/components/Youbuyit';
+
+
+type Props = {
+    params: {
+        id: string,
+    }
+}
+
+
+export default function Phone({params} : Props){
+
+    const tablet = useSelector(state => state.goods);
+    const dispatch = useDispatch();
+    const [buy, setBuy] = useState(false);
+
+    const addOrder  = (item) => { 
+        setBuy(true);
+        dispatch(addToBasket([
+            {
+                id: item.id,
+                category:item.category,
+                name: item.name,
+                model: item.model,
+                photo: item.photo,
+                price: item.price
+            },
+        ],
+    ));
+
+    };
+
+    const onRequest=()=>{
+        setBuy(false);
+        getItem("tablets",params.id)
+        .then((data)=>{
+            dispatch(setGoods({
+                id: params.id,
+                category: data.category,
+                name: data.name,
+                model: data.model,
+                description:data.description,
+                photo: data.photo,
+                price: data.price
+            }))
+        })
+    }
+    useEffect(()=>{
+         onRequest();
+    },[])
+
+    return (
+    <>
+    <div className={styles.wrapp}>
+        {buy ? <YouBuyIT good = {tablet}/> :  <>
+
+        <img src={tablet.photo} alt={tablet.name} className={styles.photo} />
+        <div className={styles.name}>{tablet.name} {tablet.model}</div>
+        <div className={styles.description}>{tablet.description}</div>
+        <div className={styles.buy}><button className={styles.buybtn} onClick={()=>{
+            addOrder({id: params.id,
+                    category: tablet.category,
+                     name: tablet.name,
+                     model: tablet.model,
+                     photo:tablet.photo,
+                     price: tablet.price})}}>BUY {tablet.price}$</button></div>
+        </>}
+        </div> 
+    </>
+    )
+}
--- app/tablets/page.tsx
+++ app/tablets/page.tsx
@@ -1,5 +1,38 @@
-export default function Tablets(){
-    return(
-        <h1>Tablets</h1>
-    )
+"use client"
+import { useEffect, useState } from 'react';
+import { getAllItems } from '@/services/getGoods';
+import {GoodsList} from '@/components/Goods';
+import {Search} from '@/components/Search';
+
+
+export default function Phones(){
+  const [tablets, setTablets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const onRequest = () =>{
+    getAllItems('tablets')
+    .then(setTablets)
+    .finally(()=>setLoading(false));
+  }
+
+  const getBySearch =(param : string)=>{
+    if (!param) {
+      onRequest();
+    }
+    return tablets.filter((item)=>
+        item.model.toLowerCase().includes(param) 
+    )}
+
+    useEffect(()=>{
+      onRequest()
+    },[])
+    
+    return (
+    <>
+       <h1>Tablets</h1>
+       <Search getBySearch = {getBySearch} onSearch = {setTablets}/>
+       {loading ? <h3>Loading...</h3> : <GoodsList goods = {tablets}/>}
+       
+    </>
+  )
 }
