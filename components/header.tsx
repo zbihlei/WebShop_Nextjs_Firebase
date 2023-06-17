5c78eef28f8d4acd59c516d9cf92c1320f60e320
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-06-17 14:19:17 +0300

    fix bugs and styles

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,13 @@
 import Link from "next/link";
 import styles from '../app/styles/header.module.scss';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AppleIcon from '@mui/icons-material/Apple';
 
 const TheHeader: React.FC = () => {
   return (
     <header className={styles.headerwrapp}>
       <div className={styles.left}>
-        <Link href={'/'} className={styles.mplink}>webshop</Link>
+        <Link href={'/'} className={styles.mplink}><AppleIcon className={styles.apple}/> WEBSHOP</Link>
       </div>
       <div className={styles.right}>
         <Link href={'/basket'}>
